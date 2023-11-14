61d7f5c850e99aa8e746dbbc52e56006340be67c
PabloTorres25 <a01753412@tec.mx>
2023-11-14 21:07:35 +0000

    2023-11-14 03:07pm

--- Actividad Integradora/intento.py
+++ Actividad Integradora/intento.py
@@ -1,8 +1,8 @@
-from mesa.visualization.modules import CanvasGrid
-from mesa.visualization.ModularVisualization import ModularServer
-from mesa import Agent, Model 
+from mesa import Agent, Model
 from mesa.space import SingleGrid
 from mesa.time import RandomActivation
+from mesa.visualization.modules import CanvasGrid
+from mesa.visualization.ModularVisualization import ModularServer
 
 class YourAgent(Agent):
     def __init__(self, unique_id, model):
@@ -13,6 +13,8 @@ class YourModel(Model):
         self.grid = SingleGrid(width, height, True)
         self.schedule = RandomActivation(self)
 
+        # Crea tus agentes y colócalos en la grid aquí según sea necesario
+
 def agent_portrayal(agent):
     portrayal = {
         "Shape": "rect",
@@ -22,13 +24,22 @@ def agent_portrayal(agent):
         "w": 1,
         "h": 1
     }
+
+    x, y = agent.pos
+    if x == 2:  # Remarcar la línea media horizontalmente
+        portrayal["Color"] = "red"  # Cambiar el borde a negro
+        portrayal["stroke_width"] = 2  # Aumentar el ancho del borde
+
     return portrayal
 
 if __name__ == "__main__":
-    ancho = 10
-    alto = 10
+    width = 4
+    height = 4
 
-    grid = CanvasGrid(agent_portrayal, ancho, alto, 500, 500)
-    server = ModularServer(YourModel, [grid], "Grid Azul", {"width": ancho, "height": alto})
+    grid = CanvasGrid(agent_portrayal, width, height, 500, 500)
+    server = ModularServer(YourModel,
+                           [grid],
+                           "Grid con línea remarcada",
+                           {"width": width, "height": height})
     server.port = 8521  # El puerto por defecto
     server.launch()
