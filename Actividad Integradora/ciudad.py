c3a539207fc27ba410fce6a25afe6a36b600b867
PabloTorres25 <a01753412@tec.mx>
2023-11-14 20:02:47 +0000

    2023-11-14 02:02pm

--- Actividad Integradora/ciudad.py
+++ Actividad Integradora/ciudad.py
@@ -28,56 +28,23 @@ class Semaforo(Agent):
 
 class CiudadModel(Model):
     def __init__(self, num_autos):
-        self.grid = SingleGrid(24, 24, True)
+        self.grid = SingleGrid(23, 23, True)
         self.schedule = SimultaneousActivation(self)
         self.running = True # Para la visualizacion usando navegador
         self.num_autos = num_autos
 
-        # Creación de Basura
-        coor_basura = celdas_random(width, height, por_basura)
-        for x, y in coor_basura:
-            basura = Basura((x, y), self)    # Creamos un agente en x,y, su id son sus coordenadas
-            self.grid.place_agent(basura, (x, y))        # Ahora lo colocamos
-            self.schedule.add(basura) 
-        
-        primer_limpiador = Limpiador(self.num_serie, self)
-        self.grid.place_agent(primer_limpiador, (1, 1))
-        self.schedule.add(primer_limpiador)
-        self.num_serie += 1
+        new_auto = Auto(0, self)
+        self.grid.place_agent(new_auto, (22, 22))
+        self.schedule.add(new_auto)
 
     def step(self):
-        tiempo_actual = datetime.now() - self.tiempo_inicio
-        print(f"Tiempo transcurrido: {tiempo_actual}")
-        if tiempo_actual.total_seconds() >= self.tiempo_maximo_segundos:
-            self.running = False  # Detener la simulación
-            print("Tiempo Maximo alcanzado")
-            porcentaje = (self.basura_inicial / (self.height * self.width)) * 100
-            print("Basura restante = ", self.basura_inicial)
-            print("Porcentaje de basura restante = ", porcentaje, "%")
-            return
-        
-        if self.basura_inicial <= 0:
-            self.running = False  # Detener la simulación
-            print("SE ASPIRARON TODAS LAS BASURAS")
-            print(f"En un tiempo de: {tiempo_actual}")
-            return
-
         # Hacer avanzar el modelo
         self.schedule.step()
-
-        # Creación de Limpiadores
-        if self.grid.is_cell_empty((1, 1)) and self.num_serie < self.num_agents:
-            # Crear un nuevo Limpiador
-            new_limpiador = Limpiador(self.num_serie, self)
-            self.grid.place_agent(new_limpiador, (1, 1))
-            self.schedule.add(new_limpiador)
-            self.num_serie += 1
-        
 
 if __name__ == "__main__":
     
     def agent_portrayal(agent):
-        if isinstance(agent, Limpiador):
+        if isinstance(agent, Auto):
             portrayal = {"Shape": "circle",
                         "Filled": "true",
                         "Layer": 0,
@@ -91,11 +58,13 @@ if __name__ == "__main__":
                         "r": 0.25}
         return portrayal
 
+    ancho = 24
+    alto = 24
     numero_coches = 10
     grid = CanvasGrid(agent_portrayal, ancho, alto, 500, 500)
-    server = ModularServer(LimpiadoresModel,
+    server = ModularServer(CiudadModel,
                         [grid],
                         "Ciudad Model",
-                        {"numero_autos":numero_coches})
+                        {"num_autos":numero_coches})
     server.port = 8521 # The default
     server.launch()
